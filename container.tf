1da0753d3c997d89f6d7fafa622a1febadb4c36f
Bhalgruf <bahlgruflecon@gmail.com>
2022-03-24 15:39:48 +0100

    anible.ymlOK

--- container.tf
+++ container.tf
@@ -1,11 +1,11 @@
-# Create a container WEB
+locals {
+  vms=["web","SQL"]
+}
+
+# Create containers 
 resource "docker_container" "web" {
-  name  = var.containerWEBname
+  for_each = toset (local.vms)
+  name  = each.key
   image = docker_image.dockerssh.latest
 }
 
-# Create a container SQL
-resource "docker_container" "SQL" {
-  name  = var.containerSQLname
-  image = docker_image.dockerssh.latest
-}
