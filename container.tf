de352ee82ee354b259ad0ac154f072e00b4da9f0
Bhalgruf <bahlgruflecon@gmail.com>
2022-03-24 16:15:52 +0100

    serverWEB-1

--- container.tf
+++ container.tf
@@ -1,11 +1,24 @@
 locals {
-  vms=["web","SQL"]
+  vms = {
+    "web" = {
+      port_internal = 8080
+      port_external = 8080
+    }
+    "SQL" = {
+      port_internal = 8081
+      port_external = 8081
+    }
+  }
 }
 
 # Create containers 
 resource "docker_container" "web" {
-  for_each = toset (local.vms)
-  name  = each.key
-  image = docker_image.dockerssh.latest
+  for_each = local.vms
+  name     = each.key
+  image    = docker_image.dockerssh.latest
+  ports {
+    internal = each.value.port_internal
+    external = each.value.port_external
+  }
 }
 
